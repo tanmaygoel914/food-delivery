c4165bd2422582b5f3d29ab46260ef6a7bc1612f
tanmaygoel914 <goel.tanmay914@gmail.com>
2024-07-19 20:27:16 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://goeltanmay914:tanmay123@cluster0.ef1uwnq.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://goeltanmay914:tanmay123@cluster0.ef1uwnq.mongodb.net/food-del?retryWrites=true&w=majority').then(()=>console.log("DB Connected"));
 }
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://food-ordering.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: [true]
+    }
+))
 
 //db connection
 
