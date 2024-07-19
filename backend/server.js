5ac4952a44e3cc8eb395f8db1b689276ce897200
tanmaygoel914 <goel.tanmay914@gmail.com>
2024-07-19 21:26:37 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,42 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config.js'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
-const app=express()
-// const port = process.env.PORT || 4000;
+const app = express();
+const port = process.env.PORT || 4000;
 
 //middleware
-app.use(express.json())
-app.use(cors(
-    {
-        origin: ["https://food-ordering-tawny-xi.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: [true]
-    }
-))
+app.use(express.json());
+app.use(cors({
+    origin: ["https://food-ordering-tawny-xi.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 //db connection
-
 connectDB();
 
 //api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-app.listen(port,()=>{
-    console.log(`server started on `)
-})
-// http://localhost:${port}
+app.use((req, res, next) => {
+    res.status(404).send("404 Not Found");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
