c510823f94551616a2d5a3b70863bcd23b78f892
tanmaygoel914 <goel.tanmay914@gmail.com>
2024-07-19 20:43:45 +0530

    initial commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder =async(req,res)=>{
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://food-ordering.vercel.app";
 
 try
 {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,8 @@ const App = () => {
 
   const [showLogin,setShowLogin] = useState(false)
 
+
+
   return (
     <>
     {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
