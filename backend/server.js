c931185cd59d1669e372f36fa2b10a084b07129c
tanmaygoel914 <goel.tanmay914@gmail.com>
2024-07-19 20:49:14 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://food-ordering.vercel.app"],
+        origin: ["https://food-ordering-tawny-xi.vercel.app"],
         methods: ["POST", "GET"],
         credentials: [true]
     }
